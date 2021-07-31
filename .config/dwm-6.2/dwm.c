36c5c4bfb582e99e6b8eaf9a4a374dfc37f9f996
HopeBaron <truehope9000@gmail.com>
2021-07-31 10:31:16 +0300

    Compile DWM with LG3D

--- .config/dwm-6.2/dwm.c
+++ .config/dwm-6.2/dwm.c
@@ -1625,7 +1625,7 @@ setup(void)
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMCheck], XA_WINDOW, 32,
 		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMName], utf8string, 8,
-		PropModeReplace, (unsigned char *) "dwm", 3);
+		PropModeReplace, (unsigned char *) "LG3D", 4);
 	XChangeProperty(dpy, root, netatom[NetWMCheck], XA_WINDOW, 32,
 		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	/* EWMH support per view */
